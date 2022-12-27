3c682d261bda154780cb18673e79f6d24759c66f
김가영 <namiein@naver.com>
2022-12-27 22:51:22 +0900

    init : project init

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import Home from 'pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
