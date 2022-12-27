c2acc02bfdd319fb8298725d2060baf0a38eb2ca
김가영 <namiein@naver.com>
2022-12-27 23:15:07 +0900

    feat : data 추가

---
+++ src/constant/index.ts
@@ -0,0 +1,33 @@
+import myDashboardImage from 'assets/my-dashboard.png';
+import myVocaImage from 'assets/my-voca.png';
+
+// TODO: image 추가
+export const IMAGE = {
+    1: myVocaImage,
+    2: myVocaImage,
+    3: myVocaImage,
+    4: myVocaImage,
+    5: myVocaImage,
+    6: myVocaImage,
+    7: myVocaImage,
+    8: myVocaImage,
+    9: myVocaImage,
+    10: myVocaImage,
+    11: myVocaImage,
+    12: myDashboardImage
+};
+
+export const DATA = {
+    1: ['react-trello', 'react-crypto-tracker'],
+    2: ['carrot-market', 'nomflix-tx'],
+    3: ['nest-study', 'next-study'],
+    4: ['next-ts-study', 'cadro'],
+    5: ['chart-test'],
+    6: ['css-study'],
+    7: ['next-blog'],
+    8: ['d3-study', 'blind-clone'],
+    9: ['next-ts-template', 'my-voca'],
+    10: ['my-form-builder', 'my-color-palette', 'my-sticker', 'my-audio-player', 'my-reservation'],
+    11: ['metamask', 'nomad-coders-clone-coding', 'weekly-clone-coding', 'my-editor', 'my-dashboard'],
+    12: ['play-with-canvas', 'jest-study', 'advent-of-code-2022', 'algorithms']
+};
