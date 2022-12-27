de92d92419e8828ad729f5a36effabc097714e7e
김가영 <namiein@naver.com>
2022-12-27 23:32:26 +0900

    feat : components -> index.ts 작성

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import Banner from 'components/Banner';
+import RepositoryList from 'components/RepositoryList';
+import SlideIndicator from 'components/SlideIndicator';
+import Sticker from 'components/Sticker';
+
+export { Banner, RepositoryList, SlideIndicator, Sticker };
