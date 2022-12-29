84484fb0b78e976da114d9635a79cefbccf60df8
김가영 <namiein@naver.com>
2022-12-29 20:24:15 +0900

    mod : change index

--- src/components/SlideIndicator/index.tsx
+++ src/components/SlideIndicator/index.tsx
@@ -2,7 +2,7 @@ export default function SlideIndicator({ index, onClick }: { index: number; onCl
     return (
         <div className="absolute bottom-0 flex items-center justify-center gap-[5px]">
             {Array.from({ length: 7 }).map((_, i) => (
-                <button key={i} type="button" className={`${i + 1 === index ? 'w-[24px]  bg-gray-500' : 'w-[8px] bg-gray-50'} h-[8px] rounded-md`} onClick={() => onClick(i + 1)} />
+                <button key={i} type="button" className={`${i === index ? 'w-[24px]  bg-gray-500' : 'w-[8px] bg-gray-50'} h-[8px] rounded-md`} onClick={() => onClick(i)} />
             ))}
         </div>
     );
