2bca9d26223759bfe0c653f54e0018fd2ca24050
김가영 <namiein@naver.com>
2022-12-29 19:40:18 +0900

    chore : modify data

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -12,49 +12,56 @@ export const DATA = [
     {
         num: 1,
         name: 'My Voca',
-        skills: ['React.JS', 'TypeScript', 'react-router-dom', 'Web Storage API', 'react-hook-form', 'sweetalert2', 'tailwindCSS'],
+        skills: ['React.JS', 'TypeScript', 'react-hook-form', 'sweetalert2', 'Web Storage API', 'TailwindCSS'],
+        conventions: ['ESLint', 'ESLint with TypeScript', 'ESLint with AirBnB Style Guide', 'Prettier'],
         dates: '2022.09.22 ~ 2022.10.02',
         description: 'My Daily Vocab List'
     },
     {
         num: 2,
         name: 'My Form Builder',
-        skills: ['React.JS', 'TypeScript', 'react-router-dom', 'Clipboard API', 'react-sortablejs', 'sortablejs', 'react-syntax-highlighter', 'tailwindCSS'],
+        skills: ['React.JS', 'TypeScript', 'react-sortablejs', 'react-syntax-highlighter', 'Clipboard API', 'Heroicons', 'TailwindCSS'],
+        conventions: ['ESLint', 'ESLint with TypeScript', 'ESLint with AirBnB Style Guide', 'Prettier'],
         dates: '2022.10.03 ~ 2022.10.08',
         description: 'Clone Coding Form Builder'
     },
     {
         num: 3,
         name: 'My Color Palette',
-        skills: ['React.JS', 'TypeScript', 'react-router-dom', 'Web Storage API', 'html2canvas', 'tailwindCSS'],
+        skills: ['React.JS', 'TypeScript', 'html2canvas', 'Web Storage API', 'TailwindCSS'],
+        conventions: ['ESLint', 'ESLint with TypeScript', 'ESLint with AirBnB Style Guide', 'Prettier'],
         dates: '2022.10.09 ~ 2022.10.15',
-        description: 'Generatin Custom Color Palette'
+        description: 'Generating Custom Color Palette'
     },
     {
         num: 4,
         name: 'My Sticker',
-        skills: ['Next.JS', 'React.JS', 'TypeScript', 'Canvas API', 'tailwindCSS'],
+        skills: ['Next.JS', 'React.JS', 'TypeScript', 'Canvas API', 'TailwindCSS'],
+        conventions: ['ESLint', 'ESLint with TypeScript', 'ESLint with AirBnB Style Guide', 'Prettier'],
         dates: '2022.10.16 ~ 2022.10.23',
         description: 'Clone Coding Paint'
     },
     {
         num: 5,
         name: 'My Audio Player',
-        skills: ['React.JS', 'TypeScript', 'react-router-dom', 'Web Audio API', 'tailwindCSS'],
+        skills: ['React.JS', 'TypeScript', 'Web Audio API', 'Heroicons', 'TailwindCSS'],
+        conventions: ['ESLint', 'ESLint with TypeScript', 'ESLint with AirBnB Style Guide', 'Prettier'],
         dates: '2022.10.24 ~ 2022.11.20',
         description: 'Clone Coding MP3 Player'
     },
     {
         num: 6,
         name: 'My Editor',
-        skills: ['React.JS', 'TypeScript', 'Slate.JS', 'tailwindCSS'],
+        skills: ['React.JS', 'Slate', 'Emoji Mart', 'Font Awesome', 'TailwindCSS'],
+        conventions: ['ESLint', 'ESLint with AirBnB Style Guide', 'Prettier'],
         dates: '2022.11.21 ~ 2022.12.04',
         description: 'Clone Coding Rich Text Editor'
     },
     {
         num: 7,
         name: 'My Dashboard',
-        skills: ['React.JS', 'TypeScript', 'D3.JS', 'react-router-dom', 'tailwindCSS'],
+        skills: ['React.JS', 'TypeScript', 'D3.JS', 'Font Awesome', 'TailwindCSS'],
+        conventions: ['ESLint', 'ESLint with TypeScript', 'ESLint with AirBnB Style Guide', 'Prettier'],
         dates: '2022.12.05 ~ 2022.12.31',
         description: 'Clone Coding Dashboard'
     }
